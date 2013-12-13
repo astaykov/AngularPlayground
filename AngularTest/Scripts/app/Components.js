4277e31685e954b67e96ca0ab94db09f5acd160c
Anton Staykov <astaykov@gmail.com>
2013-12-13 12:13:40 +0100

    project commit

---
+++ AngularTest/Scripts/app/Components.js
@@ -0,0 +1,121 @@
+﻿angular.module('components', [])
+
+.directive('screen', function () {
+    return {
+        restrict: 'E',
+        transclude: true,
+        controller: function ($scope, $element) {
+            var steps = $scope.steps = activityData;
+            this.addStep = function (step) {
+                steps.push(step);
+            }
+        },
+        template:
+            '<div class="step" ng-repeat="step in steps">'+
+            '       <step step="step"></step>' +
+            '</div>',
+        replace: true
+    };
+})
+
+.directive('step', function ($compile) {
+    var mainTemplate = 
+        '<div class="panel panel-primary">' +
+        '    <div class="panel-heading">{{step.title}}</div>' +
+        '    <div class="panel-body">' +
+        '       &content&' +
+        '    </div>'+
+        '</div>';
+    var textTemplate = '<bb-text data="step.data"></bb-text>';
+    var gridTemplate = '<bb-grid data="step.data">This is Grid Data</bb-grid>';
+    var bigTextTemplate = '<bb-text-area data="step.data"></bb-text-area>';
+
+    var getTemplate = function (contentType) {
+        var template = '';
+
+        switch (contentType) {
+            case 'text':
+                template = mainTemplate.replace("&content&", textTemplate);
+                break;
+            case 'bigtext':
+                template = mainTemplate.replace("&content&", bigTextTemplate);
+                break;
+            case 'grid':
+                template = mainTemplate.replace("&content&", gridTemplate);
+                break;
+        }
+        return template;
+    }
+
+    var linker = function (scope, element, attrs) {
+        console.log('entering Linker: ' + scope.step.data.type);
+        element.html(getTemplate(scope.step.data.type));
+        console.log('Linker :: compiling...');
+        $compile(element.contents())(scope);
+        console.log('Linker :: done...');
+    }
+
+    var result = {
+        require: '^screen',
+        restrict: 'E',
+        transclude: true,
+        link: linker,
+        scope: { step: '=' },
+        replace: true,
+        controller: function($scope, $element)
+        {
+            console.log('entering Step Controller: ' + $scope.step.data.type);
+        }
+    };
+    return result;
+})
+
+.directive('bbText', function () {
+    return {
+        require: '^step',
+        restrict: 'E',
+        transclude: true,
+        replace: true,
+        scope: { data: '=' },
+        controller: function($scope, $element)
+        {
+            console.log('entering Block Controller: ' + $scope.data.type);
+        },
+        template: '<input type="text" class="form-control" png-readonly="data.readonly" ng-model="data.source" />'
+    };
+})
+
+.directive('bbTextArea', function () {
+    return {
+        require: '^step',
+        restrict: 'E',
+        transclude: true,
+        replace: true,
+        scope: { data: '=' },
+        controller: function($scope, $element)
+        {
+            console.log('entering Block Controller: ' + $scope.data.type);
+        },
+        template: '<textarea class="form-control" rows="5" ng-readonly="data.readonly" ng-model="data.source"></textarea>'
+    };
+})
+
+.directive('bbGrid', function () {
+    return {
+        require: '^step',
+        restrict: 'E',
+        transclude: true,
+        replace: true,
+        scope: { data: '=' },
+        controller: function ($scope, $element) {
+            console.log('entering Grid Controller: ' + $scope.data.type);
+        },
+        template:
+                '<table class="table-hover table-bordered"><tbody>' +
+                    '<tr ng-repeat="row in data.source">' +
+                        '<td ng-repeat="col in row">' +
+                            '{{col}}' +
+                        '</td>' +
+                '</tbody></table>' 
+    };
+})
---
+++ AngularTest/Scripts/app/app.js
@@ -0,0 +1,76 @@
+﻿var activityData = [
+                {
+                    title: "Test",
+                    data:
+                        {
+                            type: 'text',
+                            readonly: false,
+                            source: 'I come from JSON'
+                        }
+                },
+                {
+                    title: "Big Text",
+                    data:
+                        {
+                            type: 'bigtext',
+                            readonly: true,
+                            source: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id venenatis arcu. Quisque bibendum risus sed turpis interdum rutrum. Fusce vel metus ac neque faucibus hendrerit sed eu leo. Vestibulum congue, nisi a pharetra faucibus, ligula urna ultricies enim, et dapibus mi quam et urna. Nunc vel pharetra nulla. Nullam eget quam malesuada dui vulputate ultrices et rhoncus lacus. Proin pulvinar dui pulvinar diam congue, sit amet vulputate ante consectetur. Cras vestibulum rhoncus arcu, nec consectetur diam viverra ullamcorper. Proin vulputate congue vulputate. Suspendisse tincidunt sapien non sapien cursus, nec blandit mi lobortis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam condimentum odio nulla, ac fringilla mi commodo nec. Nullam quis pretium nisl.'
+                        }
+                },
+                {
+                    title: "Test 12",
+                    data:
+                        {
+                            type: 'grid',
+                            readonly: false,
+                            source:
+                                [
+                                    {
+                                        title: 'One',
+                                        name: 'Workbook name'
+                                    },
+                                    {
+                                        title: 'One',
+                                        name: 'Workbook name 2'
+                                    },
+                                ]
+                        }
+                }
+];
+
+var todoApp = angular.module('activityApp', ['components']);
+
+todoApp.controller('TodoListController', function ($scope) {
+
+    $scope.todos = [
+        { text: 'learn angular', done: true },
+        { text: 'qka', done: false },
+        { text: 'rabota', done: false },
+        { text: 'build an angular app', done: false }
+    ];
+
+    $scope.addTodo = function () {
+        $scope.todos.push({ text: $scope.todoText, done: false });
+        $scope.todoText = '';
+    };
+
+
+    $scope.remaining = function () {
+        var count = 0;
+        angular.forEach($scope.todos, function (todo) {
+            count += todo.done ? 0 : 1;
+        });
+        return count;
+    };
+
+
+    $scope.archive = function () {
+        var oldTodos = $scope.todos;
+        $scope.todos = [];
+        angular.forEach(oldTodos, function (todo) {
+            if (!todo.done) $scope.todos.push(todo);
+        });
+    };
+
+    $scope.orderBy = "done";
+});
